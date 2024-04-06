2943731da4445dbdf209c441ed6e0b536d27671a
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-04-06 14:17:50 +0530

    Navbar

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Logo = () => {
+  return (
+    <NavLink to="/">
+      <Image
+        className="h-[2.25rem]"
+        src="https://www.paypalobjects.com/paypal-ui/logos/svg/paypal-mark-color.svg"
+        height={480}
+        width={480}
+        loading="lazy"
+        alt="Logo"
+      />
+    </NavLink>
+  );
+};
+
+export default Logo;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+import Logo from "./Logo";
+import Button from "./Button";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import Menu from "./Menu";
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const handleClick = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [isMenuOpen]);
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav>
+      <div className="flex justify-between items-center px-4 py-3 lg:py-5 lg:px-8 border-b border-[#e6e0d9]">
+        <div className="flex">
+          <div>
+            {/* Logo */}
+            <Logo />
+          </div>
+          <div className="navigations hidden ml-8 lg:flex gap-8 items-center text-[#001c64]">
+            {/* All Navigations */}
+            <NavLink>Personal</NavLink>
+            <NavLink>Business</NavLink>
+            <NavLink>Developer</NavLink>
+            <NavLink>Help</NavLink>
+          </div>
+        </div>
+        <div className="flex gap-3 items-center text-xs sm:text-sm lg:text-base">
+          <Button>My PayPal</Button>
+          <Button>Log out</Button>
+          <div
+            onClick={handleClick}
+            className="lg:hidden cursor-pointer h-10 w-10 grid place-items-center rounded-full duration-300 ease-in-out text-darkBlue border border-transparent hover:border-[#e6e0d9]"
+          >
+            {isMenuOpen ? <CloseIcon /> : <MenuIcon />}
+          </div>
+        </div>
+      </div>
+      {isMenuOpen && <Menu />}
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
